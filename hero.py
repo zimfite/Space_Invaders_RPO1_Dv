b6b3ae7af16091bfaa17484ed1957d84f434e7c6
TatianaMaksakova <tatzianar@gmail.com>
2023-10-24 14:35:32 +0300

    add moving hero

--- hero.py
+++ hero.py
@@ -1,10 +1,15 @@
 import pygame
 
+
 class Hero():
     def __init__(self, screen):
         '''инициалиция главного героя - космического корабля'''
-        self.image = pygame.image.load()
+        self.image = pygame.image.load("images/hero.png")
         self.rect = self.image.get_rect()
         self.screen = screen
         self.screen_rect = screen.get_rect()
+        self.rect.bottom = self.screen_rect.bottom
+        self.rect.centerx = self.screen_rect.centerx
 
+    def output_hero(self):
+        self.screen.blit(self.image, self.rect)
--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import pygame
 import sys
+from hero import Hero
 
 def start_game():
     '''основная функция для описания игры'''
     pygame.init()
-    screen = pygame.display.set_mode((800,600))
+    screen = pygame.display.set_mode((600, 900))
     pygame.display.set_caption("Самая лучшая игра")
+
+    #объекты классов
+    hero = Hero(screen)
 
     flag = True
     while flag:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_d:
+                    hero.rect.centerx += 10
+                if event.key == pygame.K_a:
+                    hero.rect.centerx -= 10
+
+        pygame.display.flip()
+        screen.fill(0)
+        hero.output_hero()
 
 start_game()
