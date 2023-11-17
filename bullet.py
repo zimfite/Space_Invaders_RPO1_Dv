c6d70b78861076223b2429661d513e3d33c215c0
zimfite <147493657+zimfite@users.noreply.github.com>
2023-11-17 09:24:00 +0300

    Update bullet.py

--- bullet.py
+++ bullet.py
@@ -2,7 +2,7 @@ import pygame
 
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, screen, hero):
-        super(Bullet, self).__init__()
+        super(Bullet, self).init()
         self.screen = screen
         self.rect = pygame.Rect(0, 0, 4, 12)
         self.color = 77, 109, 243
@@ -12,8 +12,10 @@ class Bullet(pygame.sprite.Sprite):
         self.y = float(self.rect.y)
 
     def draw_bullet(self):
+        '''отрисовка пуль через спрайты'''
         pygame.draw.rect(self.screen, self.color, self.rect)
 
     def update(self):
+        """перемещение пули вверх"""
         self.y -= self.speed
         self.rect.y = self.y
