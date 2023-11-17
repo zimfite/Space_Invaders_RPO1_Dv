9b018fc9df6a9d670f57380fb7babc277a4af1d8
zimfite <147493657+zimfite@users.noreply.github.com>
2023-11-17 09:22:29 +0300

    Update controls.py

--- control.py
+++ control.py
@@ -1,48 +1,45 @@
 import pygame
 import sys
-from bullet import Bullet
-from enemy import Enemy
 import os
+from  bullet import Bullet
+from  enemy import Enemy
 
-def events(screen, hero, bullets):
+def events(hero, screen, bullets):
+    
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                hero.move_right = True
-            if event.key == pygame.K_LEFT:
-                hero.move_left = True
-            if event.key == pygame.K_SPACE:
-                new_bullet = Bullet(screen, hero)
-                bullets.add(new_bullet)
-        elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                hero.move_right = False
-            if event.key == pygame.K_LEFT:
-                hero.move_left = False
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        hero.move_right = True
+                    if event.key == pygame.K_LEFT:
+                        hero.move_left = True
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_RIGHT:
+                        hero.move_right = False
+                    if event.key == pygame.K_LEFT:
+                        hero.move_left = False
+
 
 def update(screen, hero, bullets, enemys):
-    screen.fill(0)
+    screen.blit(pygame.image.load(os.path.join('img/space.jpg')), (-100, 100))
     for bullet in bullets.sprites():
         bullet.draw_bullet()
     hero.output_hero()
-    enemys.update()
+    enemys.output_enemy()
     pygame.display.flip()
-
-def moving_bullets(screen, bullets):
+    
+def moving_bullets(screen, bullets, enemys):
     bullets.update()
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
 
-def create_army(screen,enemys):
+    collisious = pygame.sprite.groupcollide(bullets, enemys, True, True) 
+
+def create_army(screen):
     enemy = Enemy(screen)
     enemy_wight = enemy.rect.width
 
-    num_enemy_x = int((600 - 2*enemy_wight) / enemy_wight)
-    for i in range(num_enemy_x):
-        enemy = Enemy(screen)
-        enemy.x = enemy_wight + enemy_wight * i
-        enemy.rect.x = enemy.x
-        enemys.add(enemy)
+    num_enemy_x = int(600)
