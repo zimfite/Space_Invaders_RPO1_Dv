8db84f7d84a95d7f94beed3871be4a48f5b70190
zimfite <katy2007dv@gmail.com>
2023-11-18 18:05:04 +0300

    add

--- hero.py
+++ hero.py
@@ -1,23 +1,19 @@
 import pygame
+class Hero:
+  def __init__(self, screen):
+    '''инициализация главного героя'''
+    self.image = pygame.image.load("img/hero.png")
+    self.rect = self.image.get_rect()
+    self.screen = screen
+    self.screen_rect = screen.get_rect()
+    self.rect.bottom = self.screen_rect.bottom
+    self.rect.centerx = self.screen_rect.centerx
+    self.lives = 3
+    
+  def output_hero(self):
+    self.screen.blit(self.image, self.rect)
+  def remove_life(self):
+      self.lives -= 1
 
-
-class Hero():
-    def __init__(self, screen):
-        self.image = pygame.image.load("img/hero.png")
-        self.rect = self.image.get_rect()
-        self.screen = screen
-        self.screen_rect = screen.get_rect()
-        self.rect.bottom = self.screen_rect.bottom
-        self.rect.centerx = self.screen_rect.centerx
-        self.move_right = False
-        self.move_left = False
-
-    def output_hero(self):
-        self.screen.blit(self.image, self.rect)
-
-    def moving_hero(self, screen):
-        if self.move_right and self.rect.right < self.screen_rect.right:
-            self.rect.centerx += 5
-        if self.move_left and self.rect.left > self.screen_rect.left:
-            self.rect.centerx -= 5
-
+  def get_lives(self):
+      return self.lives
--- main.py
+++ main.py
@@ -1,25 +1,162 @@
-import pygame, control
-from hero import Hero
-from pygame.sprite import Group
+import pygame 
+import sys 
+from hero import Hero 
+from enemy import Enemy 
+import random 
+ 
+class Bullet(pygame.sprite.Sprite): 
+    def __init__(self, x, y): 
+        super().__init__() 
+        self.image = pygame.image.load("img/snow.png") 
+        self.rect = self.image.get_rect() 
+        self.rect.center = (x, y) 
+        self.speed = 5 
+ 
+    def update(self): 
+        self.rect.y -= self.speed 
+ 
+def start_game(): 
+    pygame.init() 
+    screen = pygame.display.set_mode((800, 600)) 
+    pygame.display.set_caption("Самая лучшая игра для новогоднего настроения") 
+ 
+    background = pygame.image.load("img/space.jpg") 
+ 
+    hero = Hero(screen) 
+    bullets = pygame.sprite.Group()  # Create a group for bullets 
+    enemies = pygame.sprite.Group()  # Create a group for enemies 
+ 
+    clock = pygame.time.Clock() 
+    spawn_interval = 1000  # Интервал в миллисекундах между спавном врагов 
+    last_spawn_time = 0 
+    flag = True 
+ 
+    acceleration = 0.2 
+    deceleration = 0.1 
+    max_speed = 5 
+    speed_x = 0 
+    speed_y = 0 
+ 
+    # ENEMY 
+    max_enemies = 10 
+    ENEMY_WIDTH = 50 
+    ENEMY_HEIGHT = 50 
+    SCREEN_WIDTH = 800 
+ 
+    lives = 3  # Initialize the number of lives 
+    score = 0  # Initialize the score variable 
+ 
+    def spawn_enemy(): 
+        num_enemies = random.randint(1, 5)  # Случайное количество врагов от 1 до 5 
+        for _ in range(num_enemies): 
+            x = random.randint(0, SCREEN_WIDTH - ENEMY_WIDTH) 
+            y = random.randint(-ENEMY_HEIGHT, 0) 
+            enemy = Enemy(x, y) 
+            enemies.add(enemy) 
+ 
+    while flag and lives > 0: 
+        screen.blit(background, (0, 0)) 
+        for event in pygame.event.get(): 
+            if event.type == pygame.QUIT: 
+                flag = False 
+                sys.exit() 
+            elif event.type == pygame.KEYDOWN: 
+                if event.key == pygame.K_SPACE: 
+                    bullet = Bullet(hero.rect.centerx, hero.rect.top) 
+                    bullets.add(bullet)  # Add the bullet to the bullets group 
+ 
+        keys = pygame.key.get_pressed() 
+        if keys[pygame.K_a] or keys[pygame.K_a]: 
+            speed_x -= acceleration 
+        if keys[pygame.K_d] or keys[pygame.K_d]: 
+            speed_x += acceleration 
+ 
+        # Применяем замедление при отпускании клавиш 
+        if not keys[pygame.K_a] and not keys[pygame.K_a]: 
+            if speed_x < 0: 
+                speed_x += deceleration 
+        if not keys[pygame.K_d] and not keys[pygame.K_d]: 
+            if speed_x > 0: 
+                speed_x -= deceleration 
+ 
+        # Ограничиваем скорость 
+        speed_x = max(-max_speed, min(max_speed, speed_x)) 
+        speed_y = max(-max_speed, min(max_speed, speed_y)) 
+ 
+        hero.rect.x += speed_x 
+        hero.rect.y += speed_y 
+ 
+        # Обновляем и отображаем пули 
+        bullets.update() 
+        bullets.draw(screen) 
+ 
+        # Проверяем границы экрана 
+        if hero.rect.left < 0: 
+            hero.rect.left = 0 
+        if hero.rect.right > screen.get_width(): 
+            hero.rect.right = screen.get_width() 
+        if hero.rect.top < 0: 
+            hero.rect.top = 0 
+        if hero.rect.bottom > screen.get_height(): 
+            hero.rect.bottom = screen.get_height() 
+ 
+        current_time = pygame.time.get_ticks() 
+ 
+        if current_time - last_spawn_time > spawn_interval: 
+            spawn_enemy() 
+            last_spawn_time = current_time 
+ 
+        # Обновляем и отображаем врагов 
+        enemies.update() 
+        enemies.draw(screen) 
+ 
+        # Проверяем столкновение пуль с врагами 
+        collisions = pygame.sprite.groupcollide(bullets, enemies, True, True) 
+ 
+        # Увеличиваем счет при уничтожении врага 
+        score += len(collisions) 
+ 
+        # Проверяем, если враг достиг нижней границы экрана, убавляем жизнь 
+        for enemy in enemies: 
+            if enemy.rect.bottom > screen.get_height(): 
+                lives -= 1 
+                enemies.remove(enemy) 
+ 
+        # Отображаем индикатор жизней 
+        heart_image = pygame.image.load("img/heart.png")
 
-
-def start_game():
-    pygame.init()
-    screen = pygame.display.set_mode((600, 600))
-    pygame.display.set_caption("Самая лучшая игра")
-
-    hero = Hero(screen)
-    
-    bullets = Group()
-    enemys = Group()
-    
-    flag = True
-    while flag:
-        control.events(screen, hero, bullets)
-        hero.moving_hero(screen)
-        
-        control.moving_bullets(screen, bullets)
-        #control.create_army(screen)
-        control.update(screen, hero, bullets, enemys)
-
+        heart_rect = heart_image.get_rect() 
+        for i in range(lives): 
+            screen.blit(heart_image, (screen.get_width() - 10 - (i + 1) * (heart_rect.width + 5), 10)) 
+ 
+        # Отображаем счет 
+        font = pygame.font.Font(None, 36) 
+        score_text = font.render(f"Score: {score}", True, (255, 255, 255)) 
+        screen.blit(score_text, (10, 50)) 
+ 
+        hero.output_hero() 
+ 
+        pygame.display.flip() 
+        clock.tick(60) 
+ 
+    # Game over screen 
+    screen.fill((0, 0, 0))  # Fill the screen with black 
+    font = pygame.font.Font(None, 72) 
+    game_over_text = font.render("Game Over", True, (255, 0, 0)) 
+    restart_text = font.render("Press R to restart", True, (255, 255, 255)) 
+    screen.blit(game_over_text, (200, 200)) 
+    screen.blit(restart_text, (200, 300)) 
+    pygame.display.flip() 
+ 
+    while True: 
+        for event in pygame.event.get(): 
+            if event.type == pygame.QUIT: 
+                pygame.quit() 
+                sys.exit() 
+            elif event.type == pygame.KEYDOWN: 
+                if event.key == pygame.K_r: 
+                    # Restart the game 
+                    return 
+ 
+# Call the start_game function 
 start_game()
