467865f87f7388c0fa9b753df3160114f3bc9b48
zimfite <147493657+zimfite@users.noreply.github.com>
2023-11-19 19:30:44 +0300

    Update hero.py

--- hero.py
+++ hero.py
@@ -1,19 +1,18 @@
-import pygame
-class Hero:
-  def __init__(self, screen):
-    '''инициализация главного героя'''
-    self.image = pygame.image.load("img/hero.png")
-    self.rect = self.image.get_rect()
-    self.screen = screen
-    self.screen_rect = screen.get_rect()
-    self.rect.bottom = self.screen_rect.bottom
-    self.rect.centerx = self.screen_rect.centerx
-    self.lives = 3
-    
-  def output_hero(self):
-    self.screen.blit(self.image, self.rect)
-  def remove_life(self):
-      self.lives -= 1
-
-  def get_lives(self):
+import pygame 
+class Hero: 
+  def __init__(self, screen): 
+    self.image = pygame.image.load("img/hero.png") 
+    self.rect = self.image.get_rect() 
+    self.screen = screen 
+    self.screen_rect = screen.get_rect() 
+    self.rect.bottom = self.screen_rect.bottom 
+    self.rect.centerx = self.screen_rect.centerx 
+    self.lives = 3 
+     
+  def output_hero(self): 
+    self.screen.blit(self.image, self.rect) 
+  def remove_life(self): 
+      self.lives -= 1 
+ 
+  def get_lives(self): 
       return self.lives
