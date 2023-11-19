4b73b61319fc3a9695459634dc7f51596b0d29d2
zimfite <147493657+zimfite@users.noreply.github.com>
2023-11-19 19:31:07 +0300

    Update enemy.py

--- enemy.py
+++ enemy.py
@@ -1,13 +1,13 @@
-import pygame
-
-class Enemy(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        super().__init__()
-        self.image = pygame.image.load("img/m.png")
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-        self.speed = 2
-
-    def update(self):
+import pygame 
+ 
+class Enemy(pygame.sprite.Sprite): 
+    def __init__(self, x, y): 
+        super().__init__() 
+        self.image = pygame.image.load("img/m.png") 
+        self.rect = self.image.get_rect() 
+        self.rect.x = x 
+        self.rect.y = y 
+        self.speed = 2 
+ 
+    def update(self): 
         self.rect.y += self.speed
